c8743e523e4eb836c2f6ef13376c7180ad72c837
Nadir Alfakesh <nader.alfakesh@gmail.com>
2020-01-24 11:42:24 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -1,8 +1,8 @@
 function increaseCounter(){
   console.log("counter");
   const counter = document.getElementById('counter');
-  const currentCount = document.getElementById('counter').innerhtml;
-  counter.innerhtml = parseInt(currentCount)+1
+  const currentCount = document.getElementById('counter').textContent;
+  counter.textContent = parseInt(currentCount)+1
 }
 document.addEventListener("DOMContentLoaded",function(){
   const inter= setInterval(increaseCounter,1000);
