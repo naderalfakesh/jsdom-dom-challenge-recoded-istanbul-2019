e9650ac780273b1f8780193a36a5ca843e0ece52
Nadir Alfakesh <nader.alfakesh@gmail.com>
2020-01-24 11:51:10 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -8,4 +8,6 @@ document.addEventListener("DOMContentLoaded",function(){
   const inter= setInterval(() => changeCounter(true),1000);
   document.getElementById('plus').addEventListener("click" ,() => changeCounter(true) );
   document.getElementById('minus').addEventListener("click" ,() => changeCounter(false) );
+  document.getElementById('heart').addEventListener("click" ,() => console.log("heart") );
+  document.getElementById('pause').addEventListener("click" ,() => console.log("pause") );
 });
