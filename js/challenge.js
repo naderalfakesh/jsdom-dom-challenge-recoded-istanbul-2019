8868a6142aa5eee33633262352c88583feb7839b
Nadir Alfakesh <nader.alfakesh@gmail.com>
2020-01-24 11:48:10 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -1,10 +1,11 @@
-function increaseCounter(){
+function changeCounter(up){
   // console.log("counter");
   const counter = document.getElementById('counter');
   const currentCount = document.getElementById('counter').textContent;
-  counter.textContent = parseInt(currentCount)+1
+  counter.textContent = parseInt(currentCount) + (up? 1 : -1)
 }
 document.addEventListener("DOMContentLoaded",function(){
-  const inter= setInterval(increaseCounter,1000);
-  document.getElementById('plus').addEventListener("click" ,increaseCounter );
+  const inter= setInterval(changeCounter(true),1000);
+  document.getElementById('plus').addEventListener("click" ,changeCounter(true) );
+  document.getElementById('minus').addEventListener("click" ,changeCounter(false) );
 });
