a0601c26903dc05ee86ac8729d1452759e35b369
Nadir Alfakesh <nader.alfakesh@gmail.com>
2020-01-24 12:15:52 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -19,7 +19,7 @@ function addlikes(){
 
   if(currentCount == lastLiCount){
     times = lastLi.split(" ")[4];
-    
+
     times++;
     document.getElementById(`count-${currentCount}`).textContent = `Number ${currentCount} is clicked ${times} times.`
   }
