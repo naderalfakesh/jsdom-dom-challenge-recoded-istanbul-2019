5ccfe2fa3468f6d9a022fd5bc21608cd530a94ba
Nadir Alfakesh <nader.alfakesh@gmail.com>
2020-01-24 11:55:58 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -4,10 +4,17 @@ function changeCounter(up){
   const currentCount = document.getElementById('counter').textContent;
   counter.textContent = parseInt(currentCount) + (up? 1 : -1)
 }
+
+function addlikes(){
+  const currentCount = document.getElementById('counter').textContent;
+  const li = document.createElement("li");
+  li.textContent= "nader"
+  document.getElementsByClassName('likes')[0].appendChild(li);
+}
 document.addEventListener("DOMContentLoaded",function(){
   const inter= setInterval(() => changeCounter(true),1000);
   document.getElementById('plus').addEventListener("click" ,() => changeCounter(true) );
   document.getElementById('minus').addEventListener("click" ,() => changeCounter(false) );
-  document.getElementById('heart').addEventListener("click" ,() => console.log("heart") );
+  document.getElementById('heart').addEventListener("click" ,() => addlikes() );
   document.getElementById('pause').addEventListener("click" ,() => console.log("pause") );
 });
